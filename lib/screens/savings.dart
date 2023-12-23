a84b5bcd3a0e0a63e21b75eb5acc856650922a1b
serkanok1983 <serkanok1983@yahoo.com>
2023-12-24 00:53:10 +0300

    savings ui

--- lib/screens/savings.dart
+++ lib/screens/savings.dart
@@ -1,1 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class Savings extends StatelessWidget {
+  const Savings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tasarruflarım')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              '../../assets/savings-icon.svg',
+              width: 100,
+              height: 100,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Toplam Tararruflarınız',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              '1,000.00 \$',
+              style: TextStyle(fontSize: 24, color: Colors.green),
+            ),
+            const SizedBox(height: 20),
+            PieChart(
+              PieChartData(
+                sectionsSpace: 0,
+                centerSpaceRadius: 40,
+                sections: [
+                  PieChartSectionData(
+                    color: Colors.blue,
+                    value: 30,
+                    title: 'Harcamalar',
+                    radius: 80,
+                  ),
+                  PieChartSectionData(
+                    color: Colors.green,
+                    value: 70,
+                    title: 'Tasarruflar',
+                    radius: 80,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            BarChart(
+              BarChartData(
+                alignment: BarChartAlignment.spaceAround,
+                maxY: 1000,
+                barGroups: [
+                  BarChartGroupData(
+                    x: 0,
+                    barRods: [
+                      BarChartRodData(toY: 500, color: Colors.blue),
+                    ],
+                  ),
+                  BarChartGroupData(
+                    x: 1,
+                    barRods: [
+                      BarChartRodData(toY: 1000, color: Colors.green),
+                    ],
+                  ),
+                ],
+                titlesData: FlTitlesData(
+                  leftTitles: const AxisTitles(
+                      sideTitles: SideTitles(showTitles: true)),
+                  bottomTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      getTitlesWidget: (value, meta) {
+                        switch (value.toInt()) {
+                          case 0:
+                            return const Text('Önceki Dönem');
+                          case 1:
+                            return const Text('Bu Dönem');
+                          default:
+                            return const Text('');
+                        }
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text('Yatırımlar')),
+          ],
+        ),
+      ),
+    );
+  }
+}
