9ad81107de22cd8ddbaaf90c4eccdcd5670da3a8
Hakan Güneş <enghakangunes@gmail.com>
2023-12-24 02:13:58 +0300

    firebase packages are added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:app/screens/login.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
 
 class MyApp extends StatelessWidget {
