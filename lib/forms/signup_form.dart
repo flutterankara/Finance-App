6f791917b478546fb0e57c2f4895ccf36d67a840
Hakan Güneş <enghakangunes@gmail.com>
2023-12-24 00:29:39 +0300

    login screen is added

---
+++ lib/forms/signup_form.dart
@@ -0,0 +1,131 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class SignupForm extends StatefulWidget {
+  const SignupForm({super.key, required this.changeForm});
+  final void Function() changeForm;
+
+  @override
+  State<SignupForm> createState() {
+    return _SignupFormState();
+  }
+}
+
+class _SignupFormState extends State<SignupForm> {
+  final formKey = GlobalKey<FormState>();
+  bool _isAuthenticating = false;
+  File? _enteredImage;
+  String _enteredUsername = "";
+  String _enteredEmail = "";
+  String _enteredPassword = "";
+
+  void _databaseFailMsg(String errText) {
+    if (!context.mounted) return;
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(errText)),
+    );
+  }
+
+  void _onSubmit() async {
+    bool isValid = formKey.currentState!.validate();
+    if (!isValid) return;
+
+    setState(() {
+      _isAuthenticating = true;
+    });
+
+    formKey.currentState!.save();
+
+    setState(() {
+      _isAuthenticating = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              label: Text("Full Name"),
+            ),
+            enableSuggestions: false,
+            validator: (value) {
+              if (value == null || value.trim().length < 4) {
+                return "Enter at least 4 character";
+              }
+              return null;
+            },
+            onSaved: (newValue) {
+              _enteredUsername = newValue!;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              label: Text("Email"),
+            ),
+            keyboardType: TextInputType.emailAddress,
+            autocorrect: false,
+            textCapitalization: TextCapitalization.none,
+            validator: (value) {
+              if (value == null ||
+                  !value.contains("@") ||
+                  value.split("@")[0].trim().isEmpty ||
+                  value.split("@")[1].trim().isEmpty) {
+                return "Enter a valid mail address";
+              }
+              return null;
+            },
+            onSaved: (newValue) {
+              _enteredEmail = newValue!.trim();
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              label: Text("Password"),
+            ),
+            obscureText: true,
+            keyboardType: TextInputType.text,
+            validator: (value) {
+              if (value == null || value.trim().length < 6) {
+                return "Enter at least 6 character";
+              }
+              return null;
+            },
+            onSaved: (newValue) {
+              _enteredPassword = newValue!;
+            },
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          if (_isAuthenticating) const CircularProgressIndicator(),
+          if (!_isAuthenticating)
+            ElevatedButton(
+              onPressed: () {
+                _onSubmit();
+              },
+              style: ElevatedButton.styleFrom(
+                  foregroundColor: Theme.of(context).colorScheme.onSecondary,
+                  backgroundColor: Theme.of(context).colorScheme.secondary),
+              child: const Text("Sign Up"),
+            ),
+          if (!_isAuthenticating)
+            TextButton(
+              onPressed: widget.changeForm,
+              child: Text(
+                "Already have an acoount?",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
